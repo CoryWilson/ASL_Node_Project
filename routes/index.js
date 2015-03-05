a4b4a72cb907a9ca846700f5ce5c9a4393c7658f
patrickhalton04 <patrickhalton@Patricks-MacBook-Pro-14.local>
2015-03-05 13:35:18 -0500

    mvc chnges

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Break the Ice',
-                        classname: 'home'
+  res.render('index', { title: 'Break the ice',
+                        classname: 'breaktheice.com'
                       });
+});
+
+router.get('/mountainpage',function(req,res){
+   res.render('index',{title: 'Mountain Info',
+                       classname: 'mountain'});
+
 });
 
 
