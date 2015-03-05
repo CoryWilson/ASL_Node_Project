4da04a82cccb63161fbd61c3df9715f305811a03
CoryWilson <ctwilson08@gmail.com>
2015-03-05 15:01:15 -0500

    Connect to MongoDB

--- app.js
+++ app.js
@@ -55,5 +55,18 @@ app.use(function(err, req, res, next) {
   });
 });
 
+//MongoDB Stuff
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27018/ADB';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
 
 module.exports = app;
